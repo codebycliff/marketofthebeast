bcdeeb3ae13ddb585c2c725874862840cd65d823
Cliff Braton <cliff.braton@gmail.com>
2011-08-19 23:37:31 -0500

    Improved Home page and Artist Index page, as well as other minor style changes.
    Updated the schema or the Artist model, added images for three artists, and
    added a script to setup the default data for all models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110814175507) do
+ActiveRecord::Schema.define(:version => 20110819222027) do
+
+  create_table "artists", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "bio"
+    t.string   "origin"
+    t.string   "years_active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
